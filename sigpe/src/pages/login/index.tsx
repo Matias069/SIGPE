19b7b831f13966ef65bdc52cab4583d5c3f32122
`Matheus` <`matiasabp069@gmail.com`>
2025-07-06 18:03:57 -0300

    iniciar o sistema de rotas

---
+++ sigpe/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css"
+function LoginPage() {
+    return (
+        <>
+            <h1>aaaaaaaa</h1>
+            <button className="btn btn-xl">Xlarge</button>
+        </>
+    );
+}
+
+export default LoginPage;
